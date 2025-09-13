670dd5dc1509dc23fdb4098b5be3e6b5f61d3376
Sepideh <pashayan.sepideh@gmail.com>
2025-09-13 17:48:32 +0330

    remove extra files

--- ts.py
+++
@@ -1,9 +0,0 @@
-import json
-
-s1 = {'name':'sepideh','family':'pashayan','username':402103242,'password':402103242}
-s2 = {'name':'atieh','family':'jafarzadeh','username':402103206,'password':402103206}
-
-t1 = {'name':'atieh','family':'firozeh','username':111,'password':111}
-t2 = {'name':'abdolreza','family':'rasoli','username':222,'password':2
-      
-      with open()
